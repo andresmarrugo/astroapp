bac7874b565174cf5c74fb5399282c200e9532c9
Andrés Marrugo <andrexmarrugo@gmail.com>
2023-01-19 22:34:14 -0500

    custom post

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '🐈 CoderCat Blog. a cat writing code.';
+export const SITE_DESCRIPTION = 'En mi blog encontrarás contenido sobre desarrollo de software y tecnología emergente, pensamientos y ideas sobre criptomonedas, descentralización y más. Mi objetivo es compartir mi conocimiento de una forma amigable y relajada.';
